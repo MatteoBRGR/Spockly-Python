2550f8df0ac374f7740043ff12b6749dc73ef53d
amlusc <amelie.luschnig@gmail.com>
2025-05-07 17:59:11 +0200

    add additional categories

--- src/components/BlocklyComponent.jsx
+++ src/components/BlocklyComponent.jsx
@@ -20,31 +20,93 @@ const BlocklyComponent = ({ setCode }) => {
     // Initialize Blockly workspace
     workspaceRef.current = Blockly.inject(blocklyDiv.current, {
       toolbox: `
-        <xml>
-          <category name="Examples" colour="#5C81A6">
+      <xml>
+        <category name="Examples" colour="#5C81A6">
           <block type="controls_if"></block>
           <block type="logic_compare"></block>
           <block type="math_number"></block>
           <block type="math_arithmetic"></block>
           <block type="text"></block>
           <block type="text_print"></block>
-          </category>
-          <category name="Custom Blocks" colour="#5C81A6">
-            <block type="print_hello"></block>
-            <block type="math_square"></block>
-            <block type="text_greeting"></block>
-            <block type="repeat_times"></block>
-            <block type="dropdown_color"></block>
-          </category>
-          <Category name="R-Example" colour="#5CA65C">
-            <block type="create_vector"></block>
-            <block type="plot_vector"></block>
-            <block type="rnorm_block"></block>
-          </Category>
-          <category name="Variables" colour="#A65E2E" custom="VARIABLE"></category>
-
-        </xml>
-      `,
+        </category>
+        <category name="Custom Blocks" colour="#5C81A6">
+          <block type="print_hello"></block>
+          <block type="math_square"></block>
+          <block type="text_greeting"></block>
+          <block type="repeat_times"></block>
+          <block type="dropdown_color"></block>
+        </category>
+        <category name="R-Example" colour="#5CA65C">
+          <block type="create_vector"></block>
+          <block type="plot_vector"></block>
+        </category>
+    
+        <category name="Load Data" colour="#FFA726">
+          <block type="load_csv"></block>
+          <block type="load_shapefile"></block>
+          <block type="load_raster"></block>
+          <block type="load_builtin_dataset"></block>
+          <block type="get_dataset"></block>
+        </category>
+    
+        <category name="Variables" colour="#A65E2E" custom="VARIABLE"></category>
+        <category name="Math" colour="#FF8A65">
+          <block type="sum_vector"></block>
+          <block type="vector_minus_scalar"></block>
+          <block type="square_vector"></block>
+          <block type="sqrt_vector"></block>
+          <block type="divide_values"></block>
+          <block type="math_number"></block>
+          <block type="math_arithmetic"></block>
+        </category>
+    
+        <category name="Transformations" colour="#FFD54F">
+          <block type="filter_rows"></block>
+          <block type="select_columns"></block>
+          <block type="group_by_summarise"></block>
+          <block type="subset_rows"></block>
+          <block type="subset_column_range"></block>
+        </category>
+    
+        <category name="Statistics" colour="#BA68C8">
+          <block type="calculate_mean"></block>
+          <block type="calculate_sd"></block>
+          <block type="summary_statistics"></block>
+          <block type="quantile_column"></block>
+          <block type="sorted_element_at"></block>
+        </category>
+    
+        <category name="Modeling" colour="#A1887F">
+          <block type="linear_regression"></block>
+          <block type="semivariogram"></block>
+          <block type="kriging_interpolation"></block>
+        </category>
+    
+        <category name="Geometry" colour="#4DD0E1">
+          <block type="st_centroid"></block>
+          <block type="st_transform"></block>
+          <block type="st_buffer"></block>
+        </category>
+    
+        <category name="Raster" colour="#64B5F6">
+          <block type="read_stars"></block>
+          <block type="crop_raster"></block>
+          <block type="aggregate_raster"></block>
+        </category>
+    
+        <category name="Maps" colour="#81C784">
+          <block type="plot_map"></block>
+          <block type="set_map_title"></block>
+          <block type="color_by_attribute"></block>
+        </category>
+    
+        <category name="Visualization" colour="#90A4AE">
+          <block type="print_output"></block>
+          <block type="preview_data"></block>
+          <block type="show_structure"></block>
+        </category>
+      </xml>
+    `,
     });
 
     return () => {
@@ -62,16 +124,6 @@ const BlocklyComponent = ({ setCode }) => {
     setCode(pythonCode);
   };
 
-  const generateCodeR = () => {
-    if (!workspaceRef.current) {
-      console.error("Blockly workspace is not initialized.");
-      return;
-    }
-
-    const rCode = Blockly.Generator.R.workspaceToCode(workspaceRef.current);
-    setCode(rCode);
-  };
-
   return (
     <div
       style={{
@@ -83,10 +135,7 @@ const BlocklyComponent = ({ setCode }) => {
     >
       <div ref={blocklyDiv} style={{ flex: 1, width: "100%" }} />
       <div style={{ marginTop: "0.5rem", textAlign: "center" }}>
-        <GenerateButton onClick={generateCode} label="Generate Python Code" />
-      </div>
-      <div style={{ marginTop: "0.5rem", textAlign: "center" }}>
-        <GenerateButton onClick={generateCodeR} label="Generate R Code" />
+        <GenerateButton onClick={generateCode} />
       </div>
     </div>
   );
